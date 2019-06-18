c32995a8736826818a77a3f6497dd54331adeefe
ying liu <584069777@qq.com>
2019-06-18 17:54:18 +0800

    在内部处理icon,增加自定义icon-list的功能

--- plugins/element/loader.js
+++ plugins/element/loader.js
@@ -1,4 +1,51 @@
 'use strict'
+const iconStyle=`
+@font-face {
+  font-family: 'luck7-icons';
+  src: url('#{$--font-path}/element-icons.woff') format('woff'), /* chrome, firefox */
+       url('#{$--font-path}/element-icons.ttf') format('truetype'); /* chrome, firefox, opera, Safari, Android, iOS 4.2+*/
+  font-weight: normal;
+  font-style: normal
+}
+
+[class^="el-icon-"], [class*=" el-icon-"] {
+  /* use !important to prevent issues with browser extensions that change fonts */
+  font-family: 'luck7-icons' !important;
+  speak: none;
+  font-style: normal;
+  font-weight: normal;
+  font-variant: normal;
+  text-transform: none;
+  line-height: 1;
+  vertical-align: baseline;
+  display: inline-block;
+  /* Better Font Rendering =========== */
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+@each $key, $value in $icon-list {
+  .el-icon-#{$key}:before {content: #{'"'+ $value + '"'}}
+}
+.el-icon-loading {
+  animation: rotating 2s linear infinite;
+}
+
+.el-icon--right {
+  margin-left: 5px;
+}
+.el-icon--left {
+  margin-right: 5px;
+}
+
+@keyframes rotating {
+  0% {
+    transform: rotateZ(0deg);
+  }
+  100% {
+    transform: rotateZ(360deg);
+  }
+}
+`
 module.exports = function (content, map) {
   const opts = JSON.parse(this.query.split('=')[1])
   const pathHandler = {
@@ -13,7 +60,11 @@ module.exports = function (content, map) {
     content += '@import \'~element-theme-chalk/src/base.scss\';\n'
     // 组件样式
     opts.components.forEach((item) => {
-      content += `${pathHandler.themePath(item)}\n`
+      if (item === 'icon') {
+        content += iconStyle
+      } else {
+        content += `${pathHandler.themePath(item)}\n`
+      }
     })
   } else {
     // 组件样式
